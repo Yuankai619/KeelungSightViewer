88ab888dacc55480871805258f636bc8178980d3
劉元楷 <Yuankai_@outlook.com>
2025-01-13 17:01:25 +0800

    rerun deploy

--- src/test/java/com/yuankai/keelungsightviewer/KeelungSightViewerApplicationTests.java
+++ src/test/java/com/yuankai/keelungsightviewer/KeelungSightViewerApplicationTests.java
@@ -1,6 +1,6 @@
 package com.yuankai.keelungsightviewer;
 
-import com.yuankai.keelungsightviewer.utility.ImageValidator;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
